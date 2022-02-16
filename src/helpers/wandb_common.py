52bb56a7824366238738e7daa3d8839d1c9c519f
Ozan Ozyegen <oozyegen@ryerson.ca>
2022-02-16 14:47:45 -0500

    Wandb download and upload projects in a folder

--- src/helpers/wandb_common.py
+++ src/helpers/wandb_common.py
@@ -38,3 +38,31 @@ def get_wandb_df(project_name, wandb_user='oozyegen'):
     all_df = pd.concat([name_df, config_df,summary_df], axis=1)
     return all_df
 
+def download_project(entity_name, project_name, OUT_DIR='wandb/'):
+    """ Downloads all runs in a project to a local folder
+    """
+    all_df = get_wandb_df(project_name)
+    run_ids = all_df['id'].values
+    for id in tqdm(run_ids):
+        os.makedirs(os.path.join(OUT_DIR, id))
+        call = subprocess.Popen(['wandb', 'pull', '-e', entity_name, '-p', project_name, id],
+            cwd=os.path.join(OUT_DIR, id), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output, errors = call.communicate()
+        call.wait()
+        print(output)
+        print(errors)
+
+def upload_project(entity_name, project_name, INP_DIR='wandb/'):
+    """ Syncs all runs in a folder with wandb cloud,
+    !!! Only supported for offline-runs
+    No support for online-runs (see https://github.com/wandb/client/issues/1817)
+    """
+    run_ids = os.listdir(INP_DIR)
+    for id in tqdm(run_ids):
+        run_path = os.path.join(INP_DIR, id)
+        call = subprocess.Popen(['wandb', 'sync', '--id', id, '-e', entity_name, '-p', project_name, INP_DIR],
+            cwd=os.path.join(INP_DIR, id), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output, errors = call.communicate()
+        call.wait()
+        print(output)
+        print(errors)
